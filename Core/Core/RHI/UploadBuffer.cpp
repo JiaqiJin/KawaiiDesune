c1b043a9ab205ab21c1b29b5b05992e9834d8637
kawakawaDe <fdsam@hotmail.com>
2021-10-18 11:02:44 +0100

    Ring and Linear Upload Buffer kawaii

--- Core/Core/RHI/UploadBuffer.cpp
+++ Core/Core/RHI/UploadBuffer.cpp
@@ -4,7 +4,6 @@
 namespace RHI
 {
 	UploadBuffer::UploadBuffer(ID3D12Device* device, size_t max_size_in_bytes)
-		: m_ringAllocator(max_size_in_bytes)
 	{
 		auto heapProp = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
 		auto bufferDesc = CD3DX12_RESOURCE_DESC::Buffer(max_size_in_bytes);
@@ -21,4 +20,42 @@ namespace RHI
 		ThrowIfFailed(m_buffer->Map(0, &readRange, reinterpret_cast<void**>(&m_cpuAddress)));
 		m_gpuAddress = m_buffer->GetGPUVirtualAddress();
 	}
+
+	// --------------------- Lineal Upload Buffer -------------------
+	LinealUploadBuffer::LinealUploadBuffer(ID3D12Device* device, SIZE_T max_size_in_bytes)
+		: UploadBuffer(device, max_size_in_bytes), m_linearAllocator(max_size_in_bytes)
+	{
+
+	}
+
+	void LinealUploadBuffer::Clear()
+	{
+		std::lock_guard<std::mutex> guard(m_mutex);
+		m_linearAllocator.Clear();
+	}
+
+	D3D12_GPU_VIRTUAL_ADDRESS LinealUploadBuffer::GPUAddress() const
+	{
+		return m_gpuAddress;
+	}
+
+	// --------------------- Ring Upload Buffer -------------------
+	RingUploadBuffer::RingUploadBuffer(ID3D12Device* device, SIZE_T max_size_in_bytes)
+		: UploadBuffer(device, max_size_in_bytes), m_ringAllocator(max_size_in_bytes)
+	{
+
+	}
+
+	void RingUploadBuffer::FinishCurrentFrame(uint64_t frame)
+	{
+		std::lock_guard<std::mutex> guard(m_mutex);
+		m_ringAllocator.FinishCurrentFrame(frame);
+	}
+
+	void RingUploadBuffer::ReleaseCompletedFrames(uint64_t completedFrame)
+	{
+		std::lock_guard<std::mutex> guard(m_mutex);
+		m_ringAllocator.ReleaseCompletedFrames(completedFrame);
+	}
+
 }
--- Core/Core/RHI/UploadBuffer.h
+++ Core/Core/RHI/UploadBuffer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../Utils/LinearAllocator.h"
 #include "../Utils/RingAllocator.h"
 
 using namespace Util;
@@ -11,12 +12,37 @@ namespace RHI
 	public:
 		UploadBuffer(ID3D12Device* device, size_t max_size_in_bytes);
 
-	private:
-		RingAllocator m_ringAllocator;
+	protected:
 		std::mutex m_mutex;
 
 		Microsoft::WRL::ComPtr<ID3D12Resource> m_buffer = nullptr;
 		BYTE* m_cpuAddress = nullptr;
 		D3D12_GPU_VIRTUAL_ADDRESS m_gpuAddress = 0;
+	};
+
+	// Lineal Upload Buffer
+	class LinealUploadBuffer : public UploadBuffer
+	{
+	public:
+		LinealUploadBuffer(ID3D12Device* device, SIZE_T max_size_in_bytes);
+		
+		void Clear();
+
+		D3D12_GPU_VIRTUAL_ADDRESS GPUAddress() const;
+	private:
+		LinearAllocator m_linearAllocator;
+	};
+
+	// Ring Upload Buffer
+	class RingUploadBuffer : public UploadBuffer
+	{
+	public:
+		RingUploadBuffer(ID3D12Device* device, SIZE_T max_size_in_bytes);
+
+		void FinishCurrentFrame(uint64_t frame);
+
+		void ReleaseCompletedFrames(uint64_t completedFrame);
+	private:
+		RingAllocator m_ringAllocator;
 	};
 }
