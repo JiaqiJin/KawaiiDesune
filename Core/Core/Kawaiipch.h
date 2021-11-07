577738d6e17f6f3285887746b70aeb2cd1c65c00
kawakawaDe <fdsam@hotmail.com>
2021-11-07 10:45:50 +0000

    math

--- Core/Core/Kawaiipch.h
+++ Core/Core/Kawaiipch.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#pragma warning(disable:4201) // nonstandard extension used : nameless struct/union
+#pragma warning(disable:4238) // nonstandard extension used : class rvalue used as lvalue
+#pragma warning(disable:4239) // A non-const reference may only be bound to an lvalue; assignment operator takes a reference to non-const
+#pragma warning(disable:4324) // structure was padded due to __declspec(align())
+
+#include <winsdkver.h>
+#define _WIN32_WINNT 0x0A00
+#include <sdkddkver.h>
+
 // STL
 #include <algorithm>
 #include <array>
@@ -16,13 +25,20 @@
 #include <unordered_map>
 #include <vector>
 #include <assert.h>
+#include <ppltasks.h>
 
 // DirectX
+#include <wrl/client.h>
+#include <wrl/event.h>
+
 #include <d3d12.h>
 #include <d3dcompiler.h>
 #include <dxgi1_6.h>
-#include <dxgidebug.h>
 #include <pix3.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
 
 // Windows
 #define NOMINMAX
@@ -31,6 +47,7 @@
 #define NOMCX
 #define NOSERVICE
 #define NOHELP
+
 #include <windows.h>
 #include <WindowsX.h>
 #include <wrl.h>
@@ -39,6 +56,7 @@
 #include <intrin.h>
 #include <xmmintrin.h>
 
-#include "Common/d3dx12.h"
-#include "Utils/DxException.h"
-#include "Utils/Debug.h"
+#define D3D12_GPU_VIRTUAL_ADDRESS_NULL      ((D3D12_GPU_VIRTUAL_ADDRESS)0)
+#define D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN   ((D3D12_GPU_VIRTUAL_ADDRESS)-1)
+
+#include "Math//VectorMath.h"
