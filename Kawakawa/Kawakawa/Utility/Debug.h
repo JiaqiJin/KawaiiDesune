93bb25caffd3709b9ba678bc4ad9811ebbcc31d7
kawakawaDe <fdsam@hotmail.com>
2021-11-11 12:26:51 +0000

    Debug

--- Kawakawa/Kawakawa/Utility/Debug.h
+++ Kawakawa/Kawakawa/Utility/Debug.h
@@ -1,1 +1,44 @@
-#pragma once
+﻿#pragma once
+
+#include <fstream>
+#include <string>
+
+enum class LOG_LEVEL
+{
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WARNING,
+    LOG_LEVEL_ERROR
+};
+
+class Debug
+{
+public:
+
+    static void Log(LOG_LEVEL logLevel,
+        const char* function,
+        const char* file,
+        int line,
+        const std::string& message);
+
+
+private:
+    static std::string m_Message;
+};
+
+#define LOG(message)                                                                      \
+    do                                                                                    \
+    {                                                                                     \
+        Debug::Log(LOG_LEVEL::LOG_LEVEL_INFO, __FUNCTION__, __FILE__, __LINE__, message); \
+    } while(false)
+
+#define LOG_WARNING(message)                                                                      \
+    do                                                                                    \
+    {                                                                                     \
+        Debug::Log(LOG_LEVEL::LOG_LEVEL_WARNING, __FUNCTION__, __FILE__, __LINE__, message); \
+    } while(false)
+
+#define LOG_ERROR(message)                                                                      \
+    do                                                                                    \
+    {                                                                                     \
+        Debug::Log(LOG_LEVEL::LOG_LEVEL_ERROR, __FUNCTION__, __FILE__, __LINE__, message); \
+    } while(false)
