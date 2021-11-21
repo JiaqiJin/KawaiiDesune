4bf706df06aec782d29651289c4c29a491e4ac77
kawakawaDe <fdsam@hotmail.com>
2021-11-21 17:34:41 +0000

    Desc Allocation

--- Kawakawa/Kawakawa/Graphics/DX12/DescriptorAllocator.cpp
+++ Kawakawa/Kawakawa/Graphics/DX12/DescriptorAllocator.cpp
@@ -1,7 +1,73 @@
-#include "D3Dpch.h"
 #include "DescriptorAllocator.h"
 
 namespace Kawaii::Graphics::backend::DX12
 {
+	DescriptorAllocation::DescriptorAllocation(D3D12_CPU_DESCRIPTOR_HANDLE handle, std::shared_ptr<DescriptorPage> page)
+		: m_Handle(std::move(handle)), m_Page(std::move(page))
+	{
 
+	}
+
+	DescriptorAllocation::~DescriptorAllocation()
+	{
+		if (m_Page)
+			m_Page->DiscardDescriptor(*this);
+	}
+
+	// ---------------------------- Descriptor Page ------------------------------
+	void DescriptorPage::DiscardDescriptor(DescriptorAllocation& descriptor)
+	{
+		auto offset = (descriptor.m_Handle.ptr - m_Handle.ptr) / m_IncrementSize;
+		size_t size = 1;
+
+		// TODO
+	}
+
+	std::optional<std::vector<DescriptorAllocation>> DescriptorPage::Allocate(size_t num_descriptors)
+	{
+		std::vector<DescriptorAllocation> allocation;
+
+		return allocation;
+	}
+
+	// ---------------------------- Descriptor Allocator -------------------------
+	DescriptorAllocator::DescriptorAllocator(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptorsPerHeap = 1024)
+		: m_Type(type), m_NumDescriptorPerPage(numDescriptorsPerHeap)
+	{
+
+	}
+
+	void DescriptorAllocator::Initialize(ID3D12Device* device)
+	{
+		assert(device);
+		m_Device = device;
+	}
+
+	DescriptorAllocation DescriptorAllocator::Allocate()
+	{
+		auto ret = Allocate(1);
+		return std::move(ret[0]);
+	}
+
+	std::vector<DescriptorAllocation> DescriptorAllocator::Allocate(size_t numDescriptors)
+	{
+		// if the size larget than current page size, update default page size and create large page
+		m_NumDescriptorPerPage = std::max(m_NumDescriptorPerPage, numDescriptors);
+
+		std::vector<DescriptorAllocation> allocation;
+
+		for (auto&& page : m_PagePool)
+		{
+			if (auto descriptorAlloc = page->Allocate(numDescriptors))
+				allocation = std::move(descriptorAlloc.value());
+		}
+
+		if (allocation.empty())
+		{
+			// New Page
+			// TODO
+		}
+
+		return allocation;
+	}
 }
--- Kawakawa/Kawakawa/Graphics/DX12/DescriptorAllocator.h
+++ Kawakawa/Kawakawa/Graphics/DX12/DescriptorAllocator.h
@@ -1,33 +1,74 @@
 #pragma once
+#include "D3Dpch.h"
 
 #include <optional>
 
-// the DescriptorAllocator is CPU visiable only, for those used shader visiable descriptor,
-// use the dynamic descriptor heap (in context).
+/*
+* the DescriptorAllocator is CPU visiable only, for those used shader visiable descriptor,
+* use the dynamic descriptor heap (in context).
+* CPU visible descriptors are useful for “staging” resource descriptors in CPU memory 
+* and later copied to a GPU visible descriptor heap for use in a shader.
+*/
+// https://www.3dgep.com/learning-directx-12-3/#Descriptor_Allocator
 
 namespace Kawaii::Graphics::backend::DX12
 {
 	class DescriptorPage;
 
-	struct Descriptor
+	// Allocation that is returned from the DescriptorAllocator::Allocate
+	// Stores a pointer back to the page it came from and will automatically free itself if the descriptor(s) are no longer required.
+	struct DescriptorAllocation
 	{
+		DescriptorAllocation(D3D12_CPU_DESCRIPTOR_HANDLE handle, std::shared_ptr<DescriptorPage> page);
+		DescriptorAllocation(const DescriptorAllocation& rhs) : m_Handle(rhs.m_Handle), m_Page(nullptr) {}
+		DescriptorAllocation& operator=(const DescriptorAllocation& rhs)
+		{
+			if (this != &rhs) m_Handle = rhs.m_Handle;
+			return *this;
+		}
 
+		DescriptorAllocation(DescriptorAllocation&&) = default;
+		DescriptorAllocation& operator=(DescriptorAllocation&&) = default;
+		~DescriptorAllocation();
+
+		// The base descriptor.
+		D3D12_CPU_DESCRIPTOR_HANDLE m_Handle;
+		// A pointer back to the original page where this allocation came from.
+		std::shared_ptr<DescriptorPage> m_Page;
 	};
 
-	class DescriptorPage
+	// The AllocatorPage also keeps track of the free list for the Descriptor.
+	class DescriptorPage : public std::enable_shared_from_this<DescriptorPage>
 	{
 	public:
 
+		std::optional<std::vector<DescriptorAllocation>> Allocate(size_t num_descriptors);
+		void DiscardDescriptor(DescriptorAllocation& descriptor);
 	private:
-
+		ComPtr<ID3D12DescriptorHeap> m_DescriptorHeap;
+		D3D12_CPU_DESCRIPTOR_HANDLE m_Handle{};
+		size_t m_IncrementSize;
 	};
 
+	// This is the main interface to the application for requesting descriptors. The DescriptorAllocator class manages the descriptor pages.
 	class DescriptorAllocator
 	{
 	public:
+		DescriptorAllocator(D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptorsPerHeap = 1024);
 
+		void Initialize(ID3D12Device* device);
+
+		// Allocate a number of contiguous descriptors from a CPU visible descriptor heap.
+		DescriptorAllocation Allocate();
+		std::vector<DescriptorAllocation> Allocate(size_t numDescriptor);
 	private:
+		// Device
 		ID3D12Device* m_Device{};
-		D3D12_DESCRIPTOR_HEAP_TYPE                 m_Type;
+		// Descriptor Type
+		D3D12_DESCRIPTOR_HEAP_TYPE  m_Type;
+		// Num per Page
+		size_t m_NumDescriptorPerPage;
+		//
+		std::list<std::shared_ptr<DescriptorPage>> m_PagePool;
 	};
 }
