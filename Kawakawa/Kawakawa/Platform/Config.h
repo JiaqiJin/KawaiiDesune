4f1102c7aeb5b1d54d15917f2e0c47e0b10beb48
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-02-26 11:58:57 +0000

    update

--- Kawakawa/Kawakawa/Platform/Config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-#define APP_NAME "KawaiiEngine"
-#define MSAA_WIN 0
-#define WIDTH 800
-#define HEIGHT 800
--- old/Utils/Constant.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../Core/Math/EigenMath.h"
-
-#define WINDOW_WIDTH 1024
-#define WINDOW_HEIGHT 768
-
-// Camera 
-#define CAMERA_POSITION Vector3f(-10, 10, -20)
