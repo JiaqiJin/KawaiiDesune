6c298de8b439fa7812531c27980a9267d4edb11f
“Jiaqi_Jin” <fdsam@hotmail.com>
2022-03-15 22:03:10 +0000

    update

---
+++ Kawakawa/DX12Lab/WinApplication.cpp
@@ -0,0 +1,306 @@
+#include "pch.h"
+#include "WinApplication.h"
+#include <WindowsX.h>
+
+using Microsoft::WRL::ComPtr;
+using namespace std;
+using namespace DirectX;
+
+#ifdef _DEBUG
+#define DX12_ENABLE_DEBUG_LAYER
+#endif
+
+#ifdef DX12_ENABLE_DEBUG_LAYER
+#include <dxgidebug.h>
+#pragma comment(lib, "dxguid.lib")
+#endif
+
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	// Forward hwnd on because we can get messages (e.g., WM_CREATE)
+	// before CreateWindow returns, and thus before mhMainWnd is valid.
+	return WinApplication::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
+}
+
+WinApplication* WinApplication::mApp = nullptr;
+WinApplication* WinApplication::GetApp()
+{
+	return mApp;
+}
+
+WinApplication::WinApplication(HINSTANCE hInstance)
+	: mhAppInst(hInstance)
+{
+	// Only one WinApplication can be constructed.
+	assert(mApp == nullptr);
+	mApp = this;
+}
+
+WinApplication::~WinApplication()
+{
+
+}
+
+HINSTANCE WinApplication::AppInst()const
+{
+	return mhAppInst;
+}
+
+HWND WinApplication::MainWnd()const
+{
+	return mhMainWnd;
+}
+
+bool WinApplication::Get4xMsaaState()const
+{
+	return m4xMsaaState;
+}
+
+void WinApplication::Set4xMsaaState(bool value)
+{
+	if (m4xMsaaState != value)
+	{
+		m4xMsaaState = value;
+	}
+}
+
+int WinApplication::Run()
+{
+	MSG msg = { 0 };
+
+	mTimer.Reset();
+
+	while (msg.message != WM_QUIT)
+	{
+		// If there are Window messages then process them.
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		// Otherwise, do animation/game stuff.
+		else
+		{
+			mTimer.Tick();
+
+			if (!mAppPaused)
+			{
+				CalculateFrameStats();
+				Update(mTimer);
+			}
+			else
+			{
+				Sleep(100);
+			}
+		}
+	}
+
+	UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+	return (int)msg.wParam;
+}
+
+bool WinApplication::Initialize()
+{
+	if (!InitMainWindow())
+		return false;
+
+	return true;
+}
+
+LRESULT WinApplication::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		// WM_ACTIVATE is sent when the window is activated or deactivated.  
+		// We pause the game when the window is deactivated and unpause it 
+		// when it becomes active.  
+	case WM_ACTIVATE:
+		if (LOWORD(wParam) == WA_INACTIVE)
+		{
+			mAppPaused = true;
+			mTimer.Stop();
+		}
+		else
+		{
+			mAppPaused = false;
+			mTimer.Start();
+		}
+		return 0;
+
+		// WM_SIZE is sent when the user resizes the window.  
+	case WM_SIZE:
+		// Save the new client area dimensions.
+		mClientWidth = LOWORD(lParam);
+		mClientHeight = HIWORD(lParam);
+
+		// WM_EXITSIZEMOVE is sent when the user grabs the resize bars.
+	case WM_ENTERSIZEMOVE:
+		mAppPaused = true;
+		mResizing = true;
+		mTimer.Stop();
+		return 0;
+
+		// WM_EXITSIZEMOVE is sent when the user releases the resize bars.
+		// Here we reset everything based on the new window dimensions.
+	case WM_EXITSIZEMOVE:
+		mAppPaused = false;
+		mResizing = false;
+		mTimer.Start();
+		return 0;
+
+		// WM_DESTROY is sent when the window is being destroyed.
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+		// The WM_MENUCHAR message is sent when a menu is active and the user presses 
+		// a key that does not correspond to any mnemonic or accelerator key. 
+	case WM_MENUCHAR:
+
+		// Catch this message so to prevent the window from becoming too small.
+	case WM_GETMINMAXINFO:
+		((MINMAXINFO*)lParam)->ptMinTrackSize.x = 200;
+		((MINMAXINFO*)lParam)->ptMinTrackSize.y = 200;
+		return 0;
+
+	case WM_LBUTTONDOWN:
+	case WM_MBUTTONDOWN:
+	case WM_RBUTTONDOWN:
+		OnMouseDown(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		return 0;
+	case WM_LBUTTONUP:
+	case WM_MBUTTONUP:
+	case WM_RBUTTONUP:
+		OnMouseUp(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		return 0;
+	case WM_MOUSEMOVE:
+		OnMouseMove(wParam, GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+		return 0;
+	case WM_KEYUP:
+		if (wParam == VK_ESCAPE)
+		{
+			PostQuitMessage(0);
+		}
+		else if ((int)wParam == VK_F2)
+			Set4xMsaaState(!m4xMsaaState);
+
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+bool WinApplication::InitMainWindow()
+{
+	wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MainWndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("MainWnd"), NULL };
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(0, L"RegisterClass Failed.", 0, 0);
+		return false;
+	}
+
+	// Compute window rectangle dimensions based on requested client area dimensions.
+	RECT R = { 0, 0, mClientWidth, mClientHeight };
+	AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, false);
+	int width = R.right - R.left;
+	int height = R.bottom - R.top;
+
+	mhMainWnd = CreateWindow(
+		wc.lpszClassName,
+		mMainWndCaption.c_str(),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		width,
+		height,
+		NULL,
+		NULL,
+		wc.hInstance,
+		NULL
+	);
+	mhAppInst = wc.hInstance;
+
+	if (!mhMainWnd)
+	{
+		MessageBox(0, L"CreateWindow Failed.", 0, 0);
+		return false;
+	}
+
+	ShowWindow(mhMainWnd, SW_SHOW);
+	UpdateWindow(mhMainWnd);
+
+	return true;
+}
+
+static int frameCnt = 0;
+static float timeElapsed = 0.0f;
+
+void WinApplication::CalculateFrameStats()
+{
+	// Code computes the average frames per second, and also the 
+	// average time it takes to render one frame.  These stats 
+	// are appended to the window caption bar.
+	frameCnt++;
+
+	// Compute averages over one second period.
+	if ((mTimer.TotalTime() - timeElapsed) >= 1.0f)
+	{
+		float fps = (float)frameCnt; // fps = frameCnt / 1
+		float mspf = 1000.0f / fps;
+
+		wstring fpsStr = to_wstring(fps);
+		wstring mspfStr = to_wstring(mspf);
+
+		wstring windowText = mMainWndCaption +
+			L"    fps: " + fpsStr +
+			L"   mspf: " + mspfStr;
+
+		SetWindowText(mhMainWnd, windowText.c_str());
+
+		// Reset for next average.
+		frameCnt = 0;
+		timeElapsed += 1.0f;
+	}
+}
+
+
+void WinApplication::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd);
+}
+
+void WinApplication::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void WinApplication::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_RBUTTON) != 0)
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f * static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f * static_cast<float>(y - mLastMousePos.y));
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+void WinApplication::OnKeyboardInput(const GameTimer& gt)
+{
+
+}
+
+void WinApplication::Update(const GameTimer& gt)
+{
+	OnKeyboardInput(gt);
+}
+
+
+
--- Kawakawa/KawaiiGraphic/RHI/CommandContext.h
+++ Kawakawa/KawaiiGraphic/RHI/CommandContext.h
@@ -33,7 +33,19 @@ namespace RHI
 		CommandContext(const CommandContext&) = delete;
 		CommandContext& operator=(const CommandContext&) = delete;
 
+		// Prepare to render by reserving a command list and command allocator
 		void Initialize(ID3D12Device* pDevice);
+
+		GraphicsContext& GetGraphicsContext()
+		{
+			ASSERT(m_Type != D3D12_COMMAND_LIST_TYPE_COMPUTE, "Cannot convert async compute context to graphics");
+			return reinterpret_cast<GraphicsContext&>(*this);
+		}
+
+		ComputeContext& GetComputeContext()
+		{
+			return reinterpret_cast<ComputeContext&>(*this);
+		}
 	private:
 		CommandContext(D3D12_COMMAND_LIST_TYPE type);
 		void Reset();
@@ -41,5 +53,15 @@ namespace RHI
 		ID3D12Device* m_Device;
 
 		D3D12_COMMAND_LIST_TYPE m_Type;
+	};
+
+	class GraphicsContext : public CommandContext
+	{
+
+	};
+
+	class ComputeContext : public CommandContext
+	{
+
 	};
 }
